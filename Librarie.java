d1345380528419fa9b4688c314b72d645edca2bd
Alex <alex.cochior@yahoo.com>
2022-10-05 09:19:21 +0300

    New version

--- Librarie.java
+++ Librarie.java
@@ -4,15 +4,69 @@ public class Librarie{
 	
 	public Librarie(){}
 	
+	
+	// constructor,
+	// in: un obiect de tip carte, care va fi adaugat in lista de carti
+	// out: no output, e constructor
 	public void adaugaCarte(Carte c){
 		carti[nrCarti++] = c;
 	}
 	
+	// constructor, primeste datele unei carti si instantiaza o carte, dupa care o adauga in lista de carti
+	// in: String titlu carte, String autor carte, int pret carte
+	// out: no output, e constructor
 	public void adaugaCarte(String titlu, String autor, int pret){
 		Carte c = new Carte(titlu, autor, pret);
 		carti[nrCarti++] = c;
 	}
 	
+	//metoda get pentru numarul de carti din librarie
+	public int getNrCarti(){
+		return nrCarti;
+	}
+	
+	// cauta cartea cu pretul minim si o returneaza
+	// in: no input
+	// output: un obiect de tip carte
+	public Carte getCartePretMinim(){
+		int min = 9999999;
+		int poz = -1;
+		for (int i = 0; i < nrCarti; i++){
+			if (min > carti[i].getPret()){
+				min = carti[i].getPret();
+				poz = i;
+			}
+		}
+		return (carti[poz]);
+	}
+
+	// cauta cartea cu pretul maxim si o returneaza
+	// in: no input
+	// output: un obiect de tip carte
+	public Carte getCartePretMaxim(){
+		int max = -1;
+		int poz = -1;
+		for (int i = 0; i < nrCarti; i++){
+			if (max < carti[i].getPret()){
+				max = carti[i].getPret();
+				poz = i;
+			}
+		}
+		return (carti[poz]);
+	}
+	
+	// calculeaza media preturilor din librarie
+	// in: no input
+	// output: int medie preturi
+	public int mediePreturiCarti(){
+		int sum = 0;
+		for (int i = 0; i < nrCarti; i++){
+			sum += carti[i].getPret();
+		}
+		return (sum / nrCarti);
+	}
+	
+	// metoda to string, returneaza toate detaliile cartilor intr-un format ordonat
 	public String toString(){
 		StringBuffer sb = new StringBuffer("Libraria are urmatoarele carti: \n");
 		for (int i = 0; i < nrCarti; i++){
--- Proiect1.java
+++ Proiect1.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 
 public class Proiect1 {
+	
+	public static void clearConsole(){
+		for (int i = 0; i < 100; i++)
+			System.out.println("\n");
+	}
+	
 	public static void main(String args[]) {
+		String wait;
+		String titluCarte, autorCarte;
+		int pretCarte;
+		Librarie l = new Librarie();
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int optiune = -1;
 		
 		while (optiune != 0){
+			clearConsole();
 			System.out.println("0. Iesire");
 			System.out.println("1. Afisare librarie");
 			System.out.println("2. Adaugare carte");
@@ -18,24 +29,60 @@ public class Proiect1 {
 			optiune = sc.nextInt();
 			
 			while ((optiune < 0) || (optiune > 5)) {
+				clearConsole();
 				System.out.println("Optiune incorecta!");
 				optiune = sc.nextInt();
 			}
+			clearConsole();
 			switch (optiune) {
 				case 0:
 				break;
+				
 				case 1:
+				System.out.println(l);
 				break;
+				
 				case 2:
+				System.out.println("Dati titlul cartii: ");
+				titluCarte = sc.next();
+				System.out.println("Dati autorul cartii: ");
+				autorCarte = sc.next();
+				System.out.println("Dati pretul cartii: ");
+				pretCarte = sc.nextInt();
+				
+				// Carte c = new Carte(titluCarte, autorCarte, pretCarte);
+				// l.adaugaCarte(c);
+				
+				l.adaugaCarte(titluCarte, autorCarte, pretCarte);				
 				break;
+				
 				case 3:
+				if (l.getNrCarti() > 0)
+					System.out.println("Medie preturi librarie: " + l.mediePreturiCarti());
+				else
+					System.out.println("Libraria nu are carti");
 				break;
+				
 				case 4:
+				if (l.getNrCarti() > 0)
+				System.out.println("Cea mai scumpa carte este: " + l.getCartePretMaxim());
+				else
+					System.out.println("Libraria nu are carti");
 				break;
+				
 				case 5:
+				if (l.getNrCarti() > 0)
+				System.out.println("Cea mai ieftina carte este: " + l.getCartePretMinim());
+				else
+					System.out.println("Libraria nu are carti");
 				break;
+				
 				default:
 				break;
+			}
+			if (optiune != 0){
+				System.out.println("Apasati o tasta si enter pentru a continua...");
+				wait = sc.next();
 			}
 		}
 		
